6153b2cb0447f1cf226e5972bea32b22be50886a
Alexkre4et <45433324+Alexkre4et@users.noreply.github.com>
2019-06-09 13:23:02 +0300

    project structure has been changed

--- src/resource/test.sql
+++
@@ -1,3 +0,0 @@
-USE `eshop1`;
-INSERT INTO eshop1.`order` (totalPrice, purchase_date, user_id, paid) VALUES (300.00, 'null', 2, 0);
---INSERT INTO order (totalPrice, purchase_date, user_id, paid) VALUES (10.25, '2017-10-01 17:00:03', 1, 0);
